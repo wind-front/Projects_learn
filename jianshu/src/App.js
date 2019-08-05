fb4c0825597a2dcb627d5b125cce87209ab110f5
wind-front <zhouyanghuaem@163.com>
2019-08-05 08:21:03 +0800

    更新文件

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './common/header';
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import store from './store'
 
 function App() {
   return (
     <Provider store={store}>
       <Header></Header>
+      <BrowserRouter>
+        <Route path="/" exact render={() => <div>Home</div>}></Route>
+        <Route path="/detail" exact render={() => <div>detail</div>}></Route>
+      </BrowserRouter>
     </Provider>
   );
 }
